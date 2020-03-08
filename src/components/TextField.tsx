2d07779d82249878aaef000cc0e40b571d6f1e93
King Elisha <ek.chibueze@gmail.com>
2020-03-08 13:39:36 +0100

    text-field component

---
+++ src/components/TextField.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface TextFieldProps {
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
+}
+
+export default function TextField({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  error
+}: TextFieldProps) {
+  return (
+    <div className="text-field">
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <span className="error">{error}</span>}
+    </div>
+  );
+}
